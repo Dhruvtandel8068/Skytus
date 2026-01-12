9a590a08b16e5dcd411c22e6039ca722c7a18dd7
Dhruv Tandel <dhruvtandel8068@gmail.com>
2026-01-12 12:24:01 +0530

    Add files via upload

---
+++ Assessment2.py
@@ -0,0 +1,93 @@
+# Code-1
+a = 10
+b = 20
+print("Remainder:", a % b)
+
+
+# Code-2
+num = 24
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# Code-3
+a = 10
+b = 20
+if a > b:
+    print("Larger number:", a)
+elif b > a:
+    print("Larger number:", b)
+else:
+    print("Both numbers are equal")
+
+
+# Code-4
+num = 10
+square = num ** 2
+cube = num ** 3
+print("Square:", square)
+print("Cube:", cube)
+
+
+# Code-5
+a = 10
+b = 20
+if a == b:
+    print("Both numbers are equal")
+else:
+    print("Numbers are not equal")
+
+
+# Code-6
+a = 12
+b = 10
+if a > 0 and b > 0:
+    print(True)
+else:
+    print(False)
+
+
+# Code-7
+num = 12
+result = int(num)
+print("Integer value:", result)
+
+
+# Code-8
+num_str = 10
+num_int = int(num_str)               
+result = num_int * 10
+print("Result:", result)
+
+
+# Code-9
+age = 21
+salary = 300000
+# Using AND operator
+if age >= 18 and salary >= 20000:
+    print("Eligible (Age ≥ 18 AND Salary ≥ 20000)")
+else:
+    print("Not eligible with AND condition")
+# Using OR operator
+if age >= 18 or salary >= 20000:
+    print("Eligible (Age ≥ 18 OR Salary ≥ 20000)")
+else:
+    print("Not eligible with OR condition")
+
+
+# Code-10
+a = 12
+b = 15
+if b != 0:
+    quotient = a // b
+    remainder = a % b
+
+    print("Quotient:", quotient)
+    print("Remainder:", remainder)
+else:
+    print("Division by zero is not allowed")
+
+
+
