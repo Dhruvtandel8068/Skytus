0d10aef0f159b94493d84c064f892c1290b65d59
Dhruv Tandel <dhruvtandel8068@gmail.com>
2026-01-12 12:00:12 +0530

    Add files via upload

---
+++ Assessment1.py
@@ -0,0 +1,54 @@
+# Code-1
+print("Name: Dhruv, Age: 20, City: Surat")
+
+# Code-2
+a = 10
+b = 20
+print("Sum:", a + b)
+
+# Code-3
+celsius = 10
+fahrenheit = (celsius * 9/5) + 32
+print("Temperature in Fahrenheit:", fahrenheit)
+
+# Code-4
+name = "Dhruv"
+print(name.upper())
+
+# Code-5
+birth_year =2005
+current_year = 2026
+age = current_year - birth_year
+print("Your age is:", age)
+
+# Code-6
+a = 10
+b = 20
+
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+
+# Code-7
+length = 20
+width = 15
+area = length * width
+print("Area of rectangle:", area)
+
+# Code-8
+num = 20
+if num >= 0:
+    print("Positive number")
+else:
+    print("Negative number")
+
+# Code-9
+x = 15
+y = 20
+average = (x + y) / 2
+print("Average:", average)
+
+
+
